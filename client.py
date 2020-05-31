08f2b9efe04ff3a0a1d3b4a6e859d4b91d52f228
Simon Barton <Simon5@pdx.edu>
2020-05-30 22:30:43 -0700

    Add structure to both client and server

--- client.py
+++ client.py
@@ -1,52 +1,100 @@
 import socket
 import select
 import errno
+import re
+import os
+import signal
 import sys
+import pickle
 
 HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
 PORT = 9001
 
-my_username = input("Username: ")
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
+class Client: 
+    def __init__(self):
+        # Setup connection
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((IP, PORT))
+        self.client_socket.setblocking(False)
+        self.config = {}
 
-username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-client_socket.send(username_header + username)
+        # Look for config, or setup username if no config
+        def check_for_config(self):
+            path = os.environ.get('HOME') + '/.tiny'
+            if os.path.exists(path):
 
-while True:
-    message = input(f"{my_username} > ")
+                # TODO this doesn't actually work cause save doesnt actually work yet
+                def load_config(self):
+                    with open('path','rb') as fp:
+                        self.config = pickle.load(fp)
 
-    if message:
-        message = message.encode('utf-8')
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(message_header + message)
+                try:
+                    load_config(self)
+                except Exception as e:
+                    print("Exception occured while loading client config... {0}".format(e))
 
-    try:
+                return True
+            else:
+                return False
+        
+        if check_for_config(self):
+            print("Config file detected and loaded successfully. Welcome back, {self.username}")
+        else:
+            print("No config file detected... Please setup your name.")
+            def setup_username(self):
+                my_username = ''
+                while not my_username.isalpha():
+                    my_username = input("Please enter alphabetical username: ")
+                    username = my_username.encode('utf-8')
+                return username
+            self.username = setup_username(self)
+
+
+    def run(self):
+        username_header = f"{len(self.username):<{HEADER_LENGTH}}".encode('utf-8')
+        self.client_socket.send(username_header + self.username)
         while True:
-            username_header = client_socket.recv(HEADER_LENGTH)
-            if not len(username_header):
-                print("connection closed by the server")
-                sys.exit()
+            message = input(f"{self.username} > ")
 
-            username_length = int(username_header.decode('utf-8'))
-            username = client_socket.recv(username_length).decode('utf-8')
+            if message:
+                message = message.encode('utf-8')
+                message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+                self.client_socket.send(message_header + message)
 
-            message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = client_socket.recv(message_length).decode('utf-8')
+            try:
+                while True:
+                    username_header = self.client_socket.recv(HEADER_LENGTH)
+                    if not len(username_header):
+                        print("connection closed by the server")
+                        sys.exit()
 
-            print(f"{username} > {message}")
+                    username_length = int(username_header.decode('utf-8'))
+                    username = self.client_socket.recv(username_length).decode('utf-8')
 
-    except IOError as e:
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error', str(e))
-            sys.exit()
-        continue
+                    message_header = self.client_socket.recv(HEADER_LENGTH)
+                    message_length = int(message_header.decode('utf-8').strip())
+                    message = self.client_socket.recv(message_length).decode('utf-8')
 
-    except Exception as e:
-        print('General error', str(e))
-        sys.exit
+                    print(f"{username} > {message}")
+
+            except IOError as e:
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                    print('Reading error', str(e))
+                    sys.exit()
+                continue
+
+            except Exception as e:
+                print('General error', str(e))
+                sys.exit
+
+# Handle ctrl+C crash
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    print('We should probably save a config when ctrl c is hit huh///?')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+c = Client()
+c.run()
--- server.py
+++ server.py
@@ -5,64 +5,76 @@ HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 9001
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+class Room:
+    def __init__(self, name, topic='Default'):
+        self.name = name
+        self.topic_author = 'Default'
+        self.topic = topic
+        self.members = set()
 
-server_socket.bind((IP, PORT))
-server_socket.listen()
+class Server:
+    def __init__(self):
+        # this is where load file could happen
+        self.room_list = set()
+        self.clients = {}
+        self.name_list = []
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((IP, PORT))
+        self.server_socket.listen()
 
-sockets_list = [server_socket]
+        self.sockets_list = [self.server_socket]
 
-clients = {}
+    def receive_message(self, client_socket):
+        try:
+            message_header = client_socket.recv(HEADER_LENGTH)
 
-def receive_message(client_socket):
-    try:
-        message_header = client_socket.recv(HEADER_LENGTH)
+            if not len(message_header):
+                return False
+            
+            message_length = int(message_header.decode("utf-8").strip())
+            return {"header": message_header, "data":client_socket.recv(message_length)}
 
-        if not len(message_header):
+        except:
             return False
-        
-        message_length = int(message_header.decode("utf-8").strip())
-        return {"header": message_header, "data":client_socket.recv(message_length)}
 
-    except:
-        return False
 
-print("Central server now listening...")
+    def run(self):
+        print("Central server now listening...")
+        while True:
+            read_sockets, _, exception_sockets = select.select(self.sockets_list, [], self.sockets_list)
 
-while True:
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+            for notified_socket in read_sockets:
+                if notified_socket == self.server_socket:
+                    client_socket, client_address = self.server_socket.accept()
 
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
+                    user = self.receive_message(client_socket)
+                    if user is False:
+                        continue
 
-            user = receive_message(client_socket)
-            if user is False:
-                continue
+                    self.sockets_list.append(client_socket)
+                    self.clients[client_socket] = user
+                    print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username{user['data'].decode('utf-8')}")
+                
+                else:
+                    message = self.receive_message(notified_socket)
 
-            sockets_list.append(client_socket)
+                    if message is False:
+                        print(f"Closed connection from {self.clients[notified_socket]['data'].decode('utf-8')}")
+                        self.sockets_list.remove(notified_socket)
+                        del self.clients[notified_socket]
+                        continue
 
-            clients[client_socket] = user
+                    user = self.clients[notified_socket]
+                    print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
 
-            print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username{user['data'].decode('utf-8')}")
-        
-        else:
-            message = receive_message(notified_socket)
+                    for client_socket in self.clients:
+                        if client_socket != notified_socket:
+                            client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+                
+            for notified_socket in exception_sockets:
+                self.sockets_list.remove(notified_socket)
+                del self.clients[notified_socket]
 
-            if message is False:
-                print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
-
-            user = clients[notified_socket]
-            print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
-
-            for client_socket in clients:
-                if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-        
-    for notified_socket in exception_sockets:
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
+s = Server()
+s.run()
